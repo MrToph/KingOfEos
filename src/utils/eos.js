f82f39f4756fe14e3cd7f50337af3cd43baacc80
Christoph Michel <christoph.michel.1992@gmail.com>
2018-07-16 00:38:06 +0200

    :fire: Remove unnecessary imports

--- src/utils/eos.js
+++ src/utils/eos.js
@@ -1,5 +1,3 @@
-import { checkServer } from './index'
-// import Eos from '../../eosjs/lib/eos.js'
 import Eos from 'eosjs'
 
 const protocol = process.env.EOS_NETWORK_PROTOCOL || `http`
@@ -8,8 +6,6 @@ const port = process.env.EOS_NETWORK_PORT || 8888
 const chainId =
     process.env.EOS_NETWORK_CHAINID ||
     `aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906`
-
-console.log(`eos.js in production`, protocol, host, port, chainId)
 
 const network = {
     blockchain: `eos`,
